df702e53a311abbdc4be9d3264c7702363962ce5
April Schleck <april@exclusivelyducks.com>
2024-02-05 17:57:32 -0800

    Add some yaml for an EKS cluster

---
+++ eks/create_cluster.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+CLUSTER_NAME="REPLACE_ME"
+
+AWS_DEFAULT_REGION="us-west-2"
+AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
+K8S_VERSION=1.28
+
+cd $(dirname $(readlink -f $0))
+
+aws cloudformation deploy \
+    --stack-name "karpenter-${CLUSTER_NAME}" \
+    --template-file "./cloudformation.yaml" \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides "ClusterName=${CLUSTER_NAME}"
+
+eksctl create cluster -f - <<EOF
+---
+apiVersion: eksctl.io/v1alpha5
+kind: ClusterConfig
+metadata:
+  name: ${CLUSTER_NAME}
+  region: ${AWS_DEFAULT_REGION}
+  version: "${K8S_VERSION}"
+  tags:
+    karpenter.sh/discovery: ${CLUSTER_NAME}
+
+iam:
+  withOIDC: true
+  podIdentityAssociations:
+  - namespace: "kube-system"
+    serviceAccountName: karpenter
+    roleName: ${CLUSTER_NAME}-karpenter
+    permissionPolicyARNs:
+    - arn:aws:iam::${AWS_ACCOUNT_ID}:policy/KarpenterControllerPolicy-${CLUSTER_NAME}
+
+## Optionally run on fargate or on k8s 1.23
+# Pod Identity is not available on fargate  
+# https://docs.aws.amazon.com/eks/latest/userguide/pod-identities.html
+# iam:
+#   withOIDC: true
+#   serviceAccounts:
+#   - metadata:
+#       name: karpenter
+#       namespace: "kube-system"
+#     roleName: ${CLUSTER_NAME}-karpenter
+#     attachPolicyARNs:
+#     - arn:aws:iam::${AWS_ACCOUNT_ID}:policy/KarpenterControllerPolicy-${CLUSTER_NAME}
+#     roleOnly: true
+
+iamIdentityMappings:
+- arn: "arn:aws:iam::${AWS_ACCOUNT_ID}:role/KarpenterNodeRole-${CLUSTER_NAME}"
+  username: system:node:{{EC2PrivateDNSName}}
+  groups:
+  - system:bootstrappers
+  - system:nodes
+  ## If you intend to run Windows workloads, the kube-proxy group should be specified.
+  # For more information, see https://github.com/aws/karpenter/issues/5099.
+  # - eks:kube-proxy-windows
+
+managedNodeGroups:
+- instanceType: t3.medium
+  amiFamily: AmazonLinux2
+  name: ${CLUSTER_NAME}-t3.medium
+  desiredCapacity: 1
+  minSize: 1
+  maxSize: 2
+
+addons:
+- name: eks-pod-identity-agent
+
+## Optionally run on fargate
+# fargateProfiles:
+# - name: karpenter
+#  selectors:
+#  - namespace: "kube-system"
+EOF
+
+CLUSTER_ENDPOINT="$(aws eks describe-cluster --name "${CLUSTER_NAME}" --query "cluster.endpoint" --output text)"
+KARPENTER_IAM_ROLE_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:role/${CLUSTER_NAME}-karpenter"
+
+echo "${CLUSTER_ENDPOINT}" "${KARPENTER_IAM_ROLE_ARN}"
---
+++ eks/fetch_yaml.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+CLUSTER_NAME="REPLACE_ME"
+
+helm template karpenter \
+    oci://public.ecr.aws/karpenter/karpenter \
+    --include-crds \
+    --version "v0.33.2" \
+    --namespace "kube-system" \
+    --set "settings.clusterName=${CLUSTER_NAME}" \
+    --set "settings.interruptionQueue=${CLUSTER_NAME}" \
+    --set controller.resources.requests.cpu=0.1 \
+    --set controller.resources.requests.memory=256Mi \
+    --set controller.resources.limits.cpu=1 \
+    --set controller.resources.limits.memory=1Gi \
+    | grep -v "app.kubernetes.io/managed-by: Helm" \
+    | grep -v "helm.sh/chart: " \
+    > karpenter.yaml
+
+helm template nvidia-device-plugin \
+    nvidia-device-plugin \
+    --repo "https://nvidia.github.io/k8s-device-plugin" \
+    --namespace "kube-system" \
+    --include-crds \
+    | grep -v "app.kubernetes.io/managed-by: Helm" \
+    | grep -v "helm.sh/chart: " \
+    > nvidia-device-plugin.yaml
